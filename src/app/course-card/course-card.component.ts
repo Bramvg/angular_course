d36de2615828655e05031c90824311b8579bd7e7
Bram van Gaalen <bram.van.gaalen@tennet.eu>
2025-12-20 11:28:47 +0100

    add @empty and index,count, first,last, even, odd

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -13,6 +13,9 @@ export class CourseCardComponent {
     @Input({required: true})
     course:Course;
 
+    @Input({required: true})
+    index:number;
+
     @Output('courseSelected')
     courseEmitter = new EventEmitter<Course>();
 
