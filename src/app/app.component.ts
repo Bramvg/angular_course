c51293e4097e9ccb95f277e891a7bb76231c838c
Bram van Gaalen <bram.van.gaalen@tennet.eu>
2026-01-05 15:13:51 +0100

    implemented @for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,5 +18,7 @@ export class AppComponent {
     console.log("App component - CLicked event bubbled....", course);
 
   }
-
+  trackCourse(index:number, course:Course) {
+    return course.id;
+  }
 }
